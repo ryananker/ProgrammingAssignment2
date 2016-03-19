b0a910dcebede345aa8bd62a0eacff999715c88e
Ryan A <ryana@slalom.com>
2016-03-19 11:18:13 -0700

    Updated Programming Assignment 2
    Updated code provided to generate a matrix and the cached inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: cache the inverse of a matrix to avoid computing it repeatedly
+## The program requires 2 functions
 
-## Write a short comment describing this function
+## Funtion 1: This function creates a "matrix" object that can cache its inverse
+## i - variable for the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+   i <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    i <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Funtion 2: This function computes the inverse of the "matrix" returned in Function 1 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+## Test: testing the solution requires 3 steps
+## Step 1: Create a variable of type 'matrix'
+## diag() is used to create a diagonal matrix 
+
+matrixa <- diag(2,2)
+
+## Step 2: Create a new variable using Function 1 with matrix from Step 1
+
+cachematrix <- makeCacheMatrix(matrixa)
+
+## Step 3: Generate inverse matrix with Function 2 and the variable from Step 2
+
+cacheSolve(cachematrix)
